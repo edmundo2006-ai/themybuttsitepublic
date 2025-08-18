124529b7bfd9d16c4f87d64d23f70986923716d4
Edmundo Chavez <edmundo2006@outlook.com>
2025-08-18 02:52:21 -0700

    Revert "fixes to socketio"
    This reverts commit 1ff1ed6eab66b796f24fbcfe6239cbfae516c667.

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
+import eventlet
+eventlet.monkey_patch()
+
 from dotenv import load_dotenv
+
 from themybuttsite import create_app
+from themybuttsite.extensions import socketio
+
 
 load_dotenv()
+
 app = create_app()
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
