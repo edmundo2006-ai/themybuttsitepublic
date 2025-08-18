1c71aaa0b17478d849a94a0a4539ac1bb49a707c
Edmundo Chavez <edmundo2006@outlook.com>
2025-08-18 02:52:21 -0700

    Revert "fixes"
    This reverts commit 1626728fbbd59a1295d454a7f03e5b7ae4fbed7f.

--- themybuttsite/extensions.py
+++ themybuttsite/extensions.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import create_engine
 
 # Flask extensions
-socketio = SocketIO(async_mode="gevent")
+socketio = SocketIO(async_mode="eventlet")
 session_ext = Session()
 
 # SQLAlchemy 
